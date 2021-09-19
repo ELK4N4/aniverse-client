bf7ec0bd2c8e4cb5ac0b5447578b64d9e862c0b5
ELK4N4 <elkana546@gmail.com>
2021-09-19 19:12:53 +0300

    styled home page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { Box, Button, Typography } from '@material-ui/core';
 import useStyles from './style';
+import { useHistory } from 'react-router';
 
 export default function Home() {
     const classes = useStyles();
+    const history = useHistory();
     
+    const watchClick = () => {
+        history.push('/animes');
+    }
+
     return (
-        <div className={classes.background}>
-            Sandbox
-        </div>
+        <Box display="flex" alignItems="center" justifyContent="center" className={classes.background}>
+            <div color="white">
+                <Typography color="primary" variant="h1" align="center" className={classes.title}>
+                    ברוכים הבאים
+                </Typography>
+                <Typography color="primary" variant="h5" align="center" className={classes.subtitle}>
+                    קהילת האנימה הישראלית
+                </Typography>
+                <br />
+                <Button onClick={watchClick} fullWidth variant="contained" color="primary" className={classes.watchBtn}>
+                    לצפייה
+                </Button>
+            </div>
+        </Box>
     );
 }
--- src/components/pages/Home/style.js
+++ src/components/pages/Home/style.js
@@ -3,11 +3,36 @@ import background from '../../../assets/home-background.jpg';
 
 export default makeStyles((theme) => ({
     background: {
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${background}')`,
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.82), rgba(0, 0, 0, 0.82)), url('${background}')`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       width: '100%',
       height: '100%',
-    }
-  }));
+    },
+    title: {
+      color: 'white',
+      textShadow: '0px 0px 10px #000000',
+      fontSize: 80,
+      [theme.breakpoints.down('xs')]: {
+        fontSize: 60,
+      },
+    },
+    subtitle: {
+      color: 'white',
+      textShadow: '0px 0px 10px #000000',
+      [theme.breakpoints.down('sm')]: {
+        fontSize: 20,
+      },
+    },
+    watchBtn: {
+      color: 'white',
+      padding: theme.spacing(1.5),
+      fontSize: 20,
+      fontWeight: 'bold',
+      [theme.breakpoints.down('sm')]: {
+        padding: theme.spacing(.5),
+        fontSize: 20,
+      },
+    },
+}));
