9229c2822dcabe2ff30a469ea6f36b45961d9a95
ELK4N4 <elkana546@gmail.com>
2021-09-19 15:06:38 +0300

    changed user/:userId get to users/:userId in api.index.js

--- src/api/index.js
+++ src/api/index.js
@@ -18,7 +18,7 @@ export const addAnime = (newAnime) => API.post('/animes', newAnime);
 export const updateAnime = (animeId, updatedAnime) => API.put('/animes/' + animeId, updatedAnime);
 export const deleteAnime = (animeId) => API.delete('/animes/' + animeId);
 
-export const fetchFansubMembers = (fansubId) => API.get(`fansubs/${fansubId}/members`);
+export const fetchFansubMembers = (fansubId) => API.get(`/fansubs/${fansubId}/members`);
 
 export const fetchFansubs = (keyword) => API.get('/fansubs', { params: {search: keyword} });
 export const fetchFansub = (fansubId) => API.get('/fansubs/' + fansubId);
@@ -27,18 +27,18 @@ export const fetchFansubProjects = (fansubId) => API.get('/fansubs/' + fansubId 
 export const addFansub = (newFansub) => API.post('/fansubs', newFansub);
 export const updateFansub = (fansubId, updatedFansub) => API.put('/fansubs/' + fansubId, updatedFansub);
 
-export const followFansub = (fansubId) => API.post(`fansubs/${fansubId}/followers/`);
-export const unfollowFansub = (fansubId) => API.delete(`fansubs/${fansubId}/followers/`);
+export const followFansub = (fansubId) => API.post(`/fansubs/${fansubId}/followers/`);
+export const unfollowFansub = (fansubId) => API.delete(`/fansubs/${fansubId}/followers/`);
 
 export const fetchProject = (fansubId, projectId) => API.get('/fansubs/' + fansubId + '/projects/' + projectId);
-export const addProject = (fansubId, newProject) => API.post(`fansubs/${fansubId}/projects`, newProject);
-export const deleteProject = (fansubId, projectId) => API.delete(`fansubs/${fansubId}/projects/${projectId}`);
+export const addProject = (fansubId, newProject) => API.post(`/fansubs/${fansubId}/projects`, newProject);
+export const deleteProject = (fansubId, projectId) => API.delete(`/fansubs/${fansubId}/projects/${projectId}`);
 
 export const fetchEpisodes = (animeId) => API.get(`/animes/${animeId}/episodes`);
 export const fetchEpisode = (animeId, episodeId) => API.get(`/animes/${animeId}/episodes/${episodeId}`);
-export const addEpisode = (fansubId, projectId, newEpisode) => API.post(`fansubs/${fansubId}/projects/${projectId}/episodes`, newEpisode);
+export const addEpisode = (fansubId, projectId, newEpisode) => API.post(`/fansubs/${fansubId}/projects/${projectId}/episodes`, newEpisode);
 export const updateEpisode = (fansubId, projectId, episodeId, updatedEpisode) => API.put(`fansubs/${fansubId}/projects/${projectId}/episodes/${episodeId}`, updatedEpisode);
-export const deleteEpisode = (fansubId, projectId, episodeId) => API.delete(`fansubs/${fansubId}/projects/${projectId}/episodes/${episodeId}`);
+export const deleteEpisode = (fansubId, projectId, episodeId) => API.delete(`/fansubs/${fansubId}/projects/${projectId}/episodes/${episodeId}`);
 
 export const fetchComments = (animeId, episodeId) => API.get(`/animes/${animeId}/episodes/${episodeId}/comments`);
 export const addComment = (animeId, episodeId, comment) => API.post(`/animes/${animeId}/episodes/${episodeId}/comments`, comment);
@@ -46,11 +46,11 @@ export const removeComment = (animeId, episodeId, commentId) => API.delete(`/ani
 export const updateComment = (animeId, episodeId, commentId, updatedComment) => API.put(`/animes/${animeId}/episodes/${episodeId}/comments/${commentId}`, updatedComment);
 
 
-export const addMember = (fansubId, username) => API.post(`fansubs/${fansubId}/members/${username}`);
-export const removeMember = (fansubId, userId) => API.delete(`fansubs/${fansubId}/members/${userId}`);
-export const updateMember = (fansubId, userId, updatedMember) => API.put(`fansubs/${fansubId}/members/${userId}`, updatedMember);
+export const addMember = (fansubId, username) => API.post(`/fansubs/${fansubId}/members/${username}`);
+export const removeMember = (fansubId, userId) => API.delete(`/fansubs/${fansubId}/members/${userId}`);
+export const updateMember = (fansubId, userId, updatedMember) => API.put(`/fansubs/${fansubId}/members/${userId}`, updatedMember);
 
-export const fetchUser = (userId) => API.get(`user/${userId}/`);
+export const fetchUser = (userId) => API.get(`/users/${userId}/`);
 
 export const login = (formData) => API.post('/auth/login', formData);
 export const register = (formData) => API.post('/auth/register', formData);
