0aae71a43a51a62650f0047621211f8c35c3d46e
ELK4N4 <elkana546@gmail.com>
2022-04-10 17:05:38 +0300

    added sandbox

--- src/pages/Sandbox/index.js
+++ src/pages/Sandbox/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
 
 function Sandbox() {
     return (
